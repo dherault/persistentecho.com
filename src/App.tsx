f57ae3d314f39a172ef0fd321ca9249f39802ce7
David Hérault <dherault@gmail.com>
2025-05-19 16:44:51 +0300

    Add youtube video

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
 function App() {
   return (
-    <div className="h-screen w-screen overflow-y-auto flex flex-col items-center justify-center">
-      <h1 className="-mt-4 text-3xl md:text-4xl font-display font-bold text-center">
+    <div className="py-12 h-screen w-screen overflow-y-auto flex flex-col items-center">
+      <h1 className="text-3xl md:text-4xl font-display font-bold text-center">
         Persistent Echo
       </h1>
-      <h2 className="mt-7 text-5xl md:text-8xl font-display font-bold text-center">
+      <h2 className="mt-6 text-5xl md:text-8xl font-display font-bold text-center">
         Age Of Blocks
       </h2>
-      <a
-        href="https://store.steampowered.com/app/1700440/Age_Of_Blocks/"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <div className="mt-8 flex items-center gap-8">
         <img
-          src="/images/steam.png"
-          alt="Steam"
-          className="mt-8 w-48 rounded"
+          src="/images/backhand-index-pointing-right.png"
+          className="w-16"
         />
-      </a>
+        <a
+          href="https://store.steampowered.com/app/1700440/Age_Of_Blocks/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="/images/steam.png"
+            alt="Steam"
+            className="w-48 rounded"
+          />
+        </a>
+        <img
+          src="/images/backhand-index-pointing-right.png"
+          className="w-16 scale-x-[-1]"
+        />
+      </div>
+      <iframe
+        allowFullScreen
+        src="https://www.youtube.com/embed/93KAK6Rpwfc?si=CKghe4YFBkYy9vFD"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        className="mt-16 px-2 max-w-xl w-full aspect-video"
+      />
     </div>
   )
 }
